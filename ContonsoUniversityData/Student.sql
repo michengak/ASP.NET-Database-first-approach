4b4c2e1f77c8f44ed6e2d7ef7a89e2a7a151ab15
michel ngako <emoumichel@gmail.com>
2018-10-24 15:15:51 -0400

    Changing the database: Add a Column called Middle Name

--- ContonsoUniversityData/Student.sql
+++ ContonsoUniversityData/Student.sql
@@ -3,6 +3,7 @@
 	[StudentID] INT    IDENTITY (1, 1) NOT NULL,
 	[LastName]  NVARCHAR (50) NULL,
 	[FirstName]  NVARCHAR (50) NULL,
+	[MiddleName]    NVARCHAR (50) NULL,
 	[EnrollmentDate] DATETIME  NULL,
 	PRIMARY KEY CLUSTERED ([StudentID] asc)
 
